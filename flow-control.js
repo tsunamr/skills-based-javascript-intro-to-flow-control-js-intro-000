5b44916489dd33754f62c4eceedbec8423bda07d
sunam RImal Nepali <tsunamr@gmail.com>
2020-11-20 17:55:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,7 +1,6 @@
-function basicTeenager(age) {if (age>=13)
+function basicTeenager(age) {if (age>=13) && (age<=19))
 return 'You are a teenager!'}
-if (age<=19)
-return 'You are a teenager!'}
+
 
 function teenager(age) {if (age!=13-19)
 return 'You are not a teenager!'}
