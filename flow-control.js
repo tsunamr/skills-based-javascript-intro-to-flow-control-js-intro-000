499347a62cf2549e1c3c7183b5e038056c1392e7
sunam RImal Nepali <tsunamr@gmail.com>
2020-11-20 20:11:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -30,9 +30,21 @@ function ternaryTeenager(age) { return age <=12 || age >=20 ? 'You are not a tee
 
 
 function switchAge(age) { switch (age) {
-  case 13, 14, 15, 16, 17, 18, 19:
+  case 13:
+   return 'You are a teenager'
+   case 14:
+   return 'You are a teenager'
+   case 15:
+   return 'You are a teenager'
+   case 16:
+   return 'You are a teenager'
+   case 17:
+   return 'You are a teenager'
+   case 18:
+   return 'You are a teenager'
+   case 19:
+   return 'You are a teenager'
 
-    return 'You are a teenager'
   default:
   return  'You have an age'
 }
